b6b908eca750f34645e464381c647a7e201bee37
Tarun <trn18r@gmail.com>
2022-11-12 23:04:05 +0530

    build-fail-fix

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
+import { colors } from '../assets/constants';
 
 const Card = (props) => {
   return (
     <CardContainer bgColor={props.bgColor}>
-      <img
-        src={props.imgSrc}
-        style={{ height: '240px', width: '100%', objectFit: 'cover' }}
-      />
-      <h1>{props.title}</h1>
-      <div>{props.body}</div>
+      <ContentWrapper bgSrc={props.imgSrc}>
+        <Image src={props.imgSrc} />
+        <CardTitle>{props.title}</CardTitle>
+        <CardText>{props.body}</CardText>
+      </ContentWrapper>
     </CardContainer>
   );
 };
@@ -24,6 +24,39 @@ const CardContainer = styled.div`
   height: 350px;
   @media (max-width: 500px) {
     width: 100%;
+  }
+`;
+
+const Image = styled.img`
+  height: 240px;
+  width: 100%;
+  object-fit: cover;
+`;
+
+const CardTitle = styled.h1`
+  color: ${colors.textWhite};
+`;
+
+const CardText = styled.div``;
+
+const ContentWrapper = styled.div`
+  height: 100%;
+  width: 100%;
+  transition: 0.2s all ease-out;
+  &:hover {
+    background-image: url(${(props) => props.bgSrc});
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+    // opacity: 0.4;
+    background-color: rgba(0, 0, 0, 0.2);
+    ${Image} {
+      visibility: hidden;
+    }
+    ${CardTitle} {
+      opacity: 1;
+      visibility: show;
+    }
   }
 `;
 
